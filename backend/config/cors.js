dcda5584afd4f5097b32a8e5b5b8aa75aec38983
Raphael <contato.raphaelinacio@gmail.com>
2018-03-30 12:28:33 -0300

    cors config

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'http://grifoo.southcentralus.cloudapp.azure.com')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
