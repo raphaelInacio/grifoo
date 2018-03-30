a855411ec0591ee259ebb403eb00af6991fecafe
Raphael <contato.raphaelinacio@gmail.com>
2018-03-30 12:08:38 -0300

    change path backend api

--- frontend/src/app/constants/constants.ts
+++ frontend/src/app/constants/constants.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from '@angular/common/http';
 export default class Constants{
-    public url:string = "http://localhost:3003/api/v1"
+    public url:string = "http://grifoo.southcentralus.cloudapp.azure.com:3003/api/v1"
     public httpOptions = {
         headers: new HttpHeaders({ 'Content-Type': 'application/json' })
       };
