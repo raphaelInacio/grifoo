a001f338101f920528c19fbe7392762eb9a27090
Raphael <contato.raphaelinacio@gmail.com>
2018-03-30 09:28:50 -0300

    adjust prod'

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,4 +1,3 @@
-const dotenv = require('dotenv')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('./cors')
@@ -6,11 +5,10 @@ const queryParser = require('express-query-int')
 
 module.exports = function (server) {
     
-    dotenv.config();
-    
     server.use(bodyParser.urlencoded({
         extended: true
     }))
+
     server.use(bodyParser.json())
     server.use(cors)
     server.use(queryParser())
