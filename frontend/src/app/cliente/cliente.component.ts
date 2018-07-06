5dbba6e7ac1ca4cb83059d5551bfdcc364a81995
Raphael Inácio da Silva <contato.raphaelinacio@gmail.com>
2018-07-06 11:47:53 -0300

    Added validations form

--- frontend/src/app/cliente/cliente.component.ts
+++ frontend/src/app/cliente/cliente.component.ts
@@ -2,54 +2,59 @@ import { Component, OnInit, Injectable, ViewChild, Output, EventEmitter } from '
 import { ClienteService } from './services/cliente.service';
 import Cliente from '../cliente/model/cliente.model';
 import {
-  catchError,
-  map,
   tap
 } from 'rxjs/operators';
 import { NgForm } from '@angular/forms';
-import { ToasterContainerComponent, ToasterModule, ToasterService } from 'angular2-toaster';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
   styleUrls: ['./cliente.component.css'],
-  providers: [ClienteService, ToasterService],
+  providers: [ClienteService],
 })
 
 @Injectable()
 export class ClienteComponent implements OnInit {
 
-  constructor(private clienteService: ClienteService,
-    private toasterService: ToasterService) {
+  constructor(private clienteService: ClienteService) {
   }
 
   @ViewChild('formulario') public formulario: NgForm
   @Output() clienteSalvoEmiter = new EventEmitter<Cliente>();
+
   public clienteSalvo: boolean = false
-  public editarCliente: boolean = false
   private cliente: Cliente
+
+  //Error messages
+  public error: boolean = false
+  public message: string = "Campos obrigatórios não preenchidos"
+  public description: string = ""
 
   ngOnInit() {
   }
 
   public salvarDadosPessoais(): void {
 
-    let cliente: Cliente = new Cliente(this.formulario.form.value.nome, this.formulario.form.value.email, this.formulario.form.value.telefone)
+    try {
 
-    this.clienteService
-      .salvarCliente(cliente)
-      .pipe(tap((clienteResponse: Cliente) => {
-        this.cliente = clienteResponse
-        this.clienteSalvoEmiter.emit(clienteResponse)
-      }))
-      .subscribe()
+      let cliente: Cliente = new Cliente(
+        this.formulario.form.value.nome,
+        this.formulario.form.value.email,
+        this.formulario.form.value.telefone)
+
+      this.clienteService
+        .salvarCliente(cliente)
+        .pipe(tap((clienteResponse: Cliente) => {
+          this.cliente = clienteResponse
+          this.clienteSalvoEmiter.emit(clienteResponse)
+        }))
+        .subscribe()
+
+    } catch (error) {
+      this.error = true
+      this.description = error.message
+    }
+
   }
 
-  public editarCiente(): void {
-    console.log("Clique realiado")
-    this.clienteSalvo = false
-  }
 }
--- frontend/src/app/endereco/model/endereco.model.ts
+++ frontend/src/app/endereco/model/endereco.model.ts
@@ -1,11 +1,38 @@
 export default class Endereco {
-    public _id:string
+
+    public _id: string
+
     constructor(
-    public rua:string,
-    public numero:number,
-    public complemento:string,
-    public cep:string,
-    public bairro:string,
-    public cidade:string,
-    public estado:string){}
+        public rua: string,
+        public numero: number,
+        public complemento: string,
+        public cep: string,
+        public bairro: string,
+        public cidade: string,
+        public estado: string) { }
+
+    public validate(): void {
+
+        let messages: string = ""
+
+        if (this.rua.length <= 4) {
+            messages = messages.concat("Rua inválida ou não informada, ")
+        }
+
+        if (this.numero < 1) {
+            messages = messages.concat("Numero inválido ou não informado, ")
+        }
+
+        if (this.complemento.length <= 1) {
+            messages = messages.concat("Complemento inválido ou não informado, ")
+        }
+
+        if (this.bairro.length <= 4) {
+            messages = messages.concat("Bairro inválido ou não informado, ")
+        }
+
+        if (messages.length > 1) {
+            throw new Error(messages)
+        }
+    }
 }
